c843ca148afb2dba3b9aefda7edc2d726fde1eff
guerookeh <joseim2@illinois.edu>
2023-07-11 16:01:31 -0700

    implemented functioning registration frontend

--- life-tracker-main/lifetracker-page/src/components/Registration/Registration.jsx
+++ life-tracker-main/lifetracker-page/src/components/Registration/Registration.jsx
@@ -1,22 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Registration.css';
 
+import LifeTrackerAPIClient from '../../api/apiclient.js';
+
 function Registration() {
-  function handleSubmit(event) {
-    event.preventDefault();
-    const formData = new FormData(event.target);
-    const formValues = [];
-    for (const [name, value] of formData.entries()) {
-      formValues.push({ name, value });
-    }
-    // postRegistrationRequest(formValues);
+
+  const [registerStatus, setRegisterStatus] = useState(null); 
+
+  function processFormData(formData) {
+    const formValues = Object.fromEntries(formData.entries());
+    const wrappedFormValues = { credentials: formValues };
+    return wrappedFormValues;
   }
 
-  async function postRegistrationRequest(formData) {
-    // iterate through all values in the object and extract
-    // send json to the backend server to the POST /register endpoint
-    // await for a response to see if the request was successful or if request error
-    // place a loading or something below and notify the user when account successfully created
+  async function postRegister(wrappedFormValues) {
+    const apiClient = new LifeTrackerAPIClient();
+    const apiRoute = '/auth/register';
+    const response = await apiClient.post(apiRoute, wrappedFormValues, {});
+    handleRegisterStatus(response);
+  }
+
+  function handleRegisterStatus(response) {
+    if (response.ok) {
+      const successString = JSON.stringify(response.body.message);
+      setRegisterStatus(successString);
+    }
+    else {
+      const errorString = JSON.stringify(response.body.error.message);
+      setRegisterStatus(errorString);
+    }
+  }
+ 
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const wrappedFormObj = processFormData(formData); 
+    await postRegister(wrappedFormObj);
   }
 
   return (
@@ -30,13 +49,14 @@ function Registration() {
           <input name="password" type="password" placeholder="Password" />
         </div>
         <div className="form-group">
-          <input name="firstName" type="text" placeholder="First Name" />
+          <input name="first_name" type="text" placeholder="First Name" />
         </div>
         <div className="form-group">
-          <input name="lastName" type="text" placeholder="Last Name" />
+          <input name="last_name" type="text" placeholder="Last Name" />
         </div>
         <button type="submit">Register</button>
       </form>
+      { (registerStatus) ? <h3>{registerStatus}</h3> : <></> }
     </div>
   );
 }
